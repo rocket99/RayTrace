500f9d2209448375818dfbab90e21cd083230a12
rocket99 <rocket99ios@126.com>
2020-03-04 08:13:42 +0800

    add math func

--- TKMath.cpp
+++ TKMath.cpp
@@ -35,5 +35,8 @@ glm::vec3 TKMath::random_in_unit_disk(){
 	glm::vec3 p;
 	do{
 		p = 2.0f*glm::vec3(drand48(), drand48(), 0.0f) - glm::vec3(1.0, 1.0, 0.0);
-	}while(dot(p,p) >= 1.0f);
+	}while(dot(p, p) >= 1.0f);
+	return p;
 }
+
+
